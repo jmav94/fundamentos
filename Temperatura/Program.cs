bf599341339c47849a11fe2cab22e2cc5d411f5c
Juan Ahumada <39069082+jmav94@users.noreply.github.com>
2020-09-17 08:41:53 -0500

    comentarios agregados al archivo program.cs
    Se agregaron comentario de ayuda al archivo.

--- Temperatura/Program.cs
+++ Temperatura/Program.cs
@@ -6,10 +6,13 @@ namespace Temperatura
     {
         static void Main(string[] args)
         {
+            // Creacion de objeto
             TemperaturaGradosFarenheit miTemperatura = new TemperaturaGradosFarenheit();
+            // Solicitar al usuario teclear temperarutra
             Console.Write("Teclee la temperatura en grados farenheit: ");
+            // Leer temperatura
             miTemperatura.GradosFarenheit = double.Parse(Console.ReadLine());
-
+            // Imprimir resultado de metodo.
             Console.WriteLine($"La temperatura en grados centigrados: { Math.Round(miTemperatura.ConvertiAGradosCentigrados())}");
 
         }
