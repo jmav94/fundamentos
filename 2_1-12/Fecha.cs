87ee437267f704377366b8aa8a709bf0c98e3a66
Juan Ahumada <juan.ahumada94@gmail.com>
2020-09-18 11:09:13 -0500

    Ejercicio 12 de la practuca 2.1

---
+++ 2_1-12/Fecha.cs
@@ -0,0 +1,22 @@
+class Fecha
+{
+    private int _intDia;
+    public int Dia
+    {
+        get { return _intDia; }
+        set { _intDia = value; }
+    }
+    private int _intMes;
+    public int Mes
+    {
+        get { return _intMes; }
+        set { _intMes = value; }
+    }
+    private int _intAnio;
+    public int Anio
+    {
+        get { return _intAnio; }
+        set { _intAnio = value; }
+    }
+    
+}
---
+++ 2_1-12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2_1_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fecha miFecha;
+            miFecha = new Fecha();
+
+            Console.Write("Capture el dia: ");
+            miFecha.Dia = int.Parse(Console.ReadLine());
+            Console.Write("Capture el mes: ");
+            miFecha.Mes = int.Parse(Console.ReadLine());
+            Console.Write("Capture el Año: ");
+            miFecha.Anio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{miFecha.Dia}/{miFecha.Mes}/{miFecha.Anio}");
+        }
+    }
+}
