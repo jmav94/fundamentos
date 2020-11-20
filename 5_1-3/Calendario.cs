74c4fbdcc518484e6b7e9fdf59b266fff5bac6d1
Juan Ahumada <juan.ahumada94@gmail.com>
2020-11-20 02:42:18 -0600

    Ejercicio 3 de la practica 5.1

---
+++ 5_1-3/Calendario.cs
@@ -0,0 +1,28 @@
+class Calendario
+{
+    private int _intMes;
+    public int Mes
+    {
+        get { return _intMes; }
+        set { _intMes = value; }
+    }
+    public string MesConLetra(){
+        string MesL= "";
+        switch (Mes)
+        {
+            case 1: MesL = "Enero";break;
+            case 2: MesL = "Febrero";break;
+            case 3: MesL = "Marzo";break;
+            case 4: MesL = "Abril";break;
+            case 5: MesL = "Mayo";break;
+            case 6: MesL = "Junio";break;
+            case 7: MesL = "Julio";break;
+            case 8: MesL = "Agosto";break;
+            case 9: MesL = "Septiembre";break;
+            case 10: MesL = "Octubre";break;
+            case 11: MesL = "Noviembre";break;
+            case 12: MesL = "Diciembre";break;
+        }
+        return MesL;
+    }
+}
---
+++ 5_1-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calendario c = new Calendario();
+            bool blMesCorrecto = false;
+            do{
+                Console.Write("Capture el numero del mes que desea mostrar con letras: ");
+                c.Mes = int.Parse(Console.ReadLine());
+                if (c.Mes < 1 || c.Mes > 12) Console.WriteLine("Capture un mes correcto");
+                else blMesCorrecto = true;
+            }while(blMesCorrecto == false);
+            Console.WriteLine($"El mes seleccionado es {c.MesConLetra()}");
+        }
+    }
+}
